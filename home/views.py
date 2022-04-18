ef69100a83fbaa304858822feafff1deb9219003
ssj4869 <37257976+ssj4869@users.noreply.github.com>
2022-04-18 16:23:47 +0530

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+# Create your views here.
+
+def index(request):
+	data = {"Message": "Hello World"}
+	return JsonResponse(data)
